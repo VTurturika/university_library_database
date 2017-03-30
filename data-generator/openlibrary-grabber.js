b08aa9677a198bcef692f693a3adc267497d7b39
Victor Turturika <v.turturika@gmail.com>
2017-03-30 18:55:35 +0300

    Improve using Promises

--- data-generator/openlibrary-grabber.js
+++ data-generator/openlibrary-grabber.js
@@ -17,65 +17,68 @@ const getRandomArrayItem = array => array[Math.floor(Math.random()*array.length)
 const getRandomDate = (start, end) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
                                             .toISOString().slice(0, 10);
 
-function processBook(book, callback) {
+function processBook(book) {
 
-    request.get(baseUrl + book + ".json", (err, res, body) => {
+    return new Promise( (resolve, reject) => {
 
-        console.log("Processing book record " + count++);
-        let data = JSON.parse(body);
-        let processedBook = {
-            title: data.title,
-            city: data.publish_places ? data.publish_places[0] : null,
-            publisher: data.publishers ? data.publishers[0] : null,
-            year: data.publish_date ? +data.publish_date.slice(-4) : 1950,
-            subject: data.subjects ? data.subjects[0] : null,
-            genre: data.genres ? data.genres[0] : null,
-            periodic_release: getRandomArrayItem([null, null, null, null, null, 1, 2, 3]),
-            page_count: data.number_of_pages ? +data.number_of_pages : 0,
-            lang: data.languages ? data.languages[0].key.slice(11) : null,
-            days_keep_count: getRandomArrayItem([10,30,50,100,200]),
-            adding_date: getRandomDate(new Date(2000,0,1), new Date(2016,11,31)),
-            count: getRandomArrayItem([5,10,20,50,100,300,500]),
-            authors: []
-        }
+        request.get(baseUrl + book + ".json", (err, res, body) => {
 
-        if(data.authors) {
+            console.log("Processing book record " + count++);
+            let data = JSON.parse(body);
+            let processedBook = {
+                title: data.title,
+                city: data.publish_places ? data.publish_places[0] : null,
+                publisher: data.publishers ? data.publishers[0] : null,
+                year: data.publish_date ? +data.publish_date.slice(-4) : 1950,
+                subject: data.subjects ? data.subjects[0] : null,
+                genre: data.genres ? data.genres[0] : null,
+                periodic_release: getRandomArrayItem([null, null, null, null, null, 1, 2, 3]),
+                page_count: data.number_of_pages ? +data.number_of_pages : 0,
+                lang: data.languages ? data.languages[0].key.slice(11) : null,
+                days_keep_count: getRandomArrayItem([10,30,50,100,200]),
+                adding_date: getRandomDate(new Date(2000,0,1), new Date(2016,11,31)),
+                count: getRandomArrayItem([5,10,20,50,100,300,500]),
+                authors: []
+            }
 
-            let authorsPromises = [];
-            data.authors.forEach(author => {
-                authorsPromises.push(new Promise( (resolve, reject) => {
-                    processAuthor(author.key, (data) => resolve(data));
-                }));
+            if(data.authors) {
 
-            });
+                let authorsPromises = [];
+                data.authors.forEach(author => authorsPromises.push(processAuthor(author)));
 
-            Promise.all(authorsPromises).then( (data) => {
-                data.forEach(processedAuthor => {
-                    processedBook.authors.push(processedAuthor);
-                });
-                callback({key: book, data: processedBook});
-            });
-        }
-        else {
-            callback({key: book, data: processedBook});
-        }
+                Promise
+                    .all(authorsPromises)
+                    .then( data => {
+                        data.forEach(processedAuthor => {
+                            if(processedAuthor) processedBook.authors.push(processedAuthor);
+                        });
+                        resolve({key: book, data: processedBook});
+                    });
+            }
+            else {
+                resolve({key: book, data: processedBook});
+            }
+        });
     });
 }
 
-function processAuthor(author, callback) {
-    request.get(baseUrl + author + ".json", (err, res, body) => {
-        let data = JSON.parse(body);
-        let processedAuthor = null;
+function processAuthor(author) {
 
-        if(data.name) {
+    return new Promise( (resolve, reject) => {
+        request.get(baseUrl + author + ".json", (err, res, body) => {
+            let data = JSON.parse(body);
+            let processedAuthor;
 
-            processedAuthor = {
-                key: author,
-                last_name: data.name.replace(/^\S\s+/, ""),
-                first_name: data.name.replace(/(\s\S+)+$/, "")
-            };
-        }
-        callback(processedAuthor);
+            if(data.name) {
+
+                processedAuthor = {
+                    key: author,
+                    last_name: data.name.replace(/^\S\s+/, ""),
+                    first_name: data.name.replace(/(\s\S+)+$/, "")
+                };
+            }
+            resolve(processedAuthor);
+        });
     });
 }
 
@@ -86,18 +89,12 @@ request(options, (err, res, body) => {
         return;
     }
 
-
-
     if(body.status == "ok") {
 
         console.log("Fetched " + body.result.length + "records\nStart processing...");
 
         let bookPromises = [];
-        body.result.forEach(book => {
-            bookPromises.push(new Promise( (resolve, reject) => {
-                processBook(book, (data) => resolve(data));
-            }));
-        })
+        body.result.forEach(book => bookPromises.push(processBook(book)));
 
         Promise
             .all(bookPromises)
